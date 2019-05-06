699c38051803774b51ff34ba5f925bfdd9ea6c6d
Coby Jenkins <cjenkins@parchment.com>
2019-05-06 15:20:38 -0700

    02 Prove : Assignment - Digital Forensics

--- W02/myAssign02.cpp
+++ W02/myAssign02.cpp
@@ -5,25 +5,128 @@
 * Author:
 *    Coby Jenkins
 * Summary: 
-*    Enter a brief description of your program here!  Please note that if
-*    you do not take the time to fill out this block, YOU WILL LOSE POINTS.
-*    Before you begin working, estimate the time you think it will
-*    take you to do the assignment and include it in this header block.
-*    Before you submit the assignment include the actual time it took.
+*    Write a program to scan through that log to identify users who accessed
+*       files in a particular window of time.
 *
-*    Estimated:  0.0 hrs   
-*    Actual:     0.0 hrs
-*      Please describe briefly what was the most difficult part.
+*    Estimated:  4.0 hrs
+*    Actual:     3.75 hrs
+*      Trying to figure out how I was going to manage the data for the query.
 ************************************************************************/
 
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+
 using namespace std;
+
+const int MAX_BUFFER = 256;
+const int MAX_RESULTS = 500;
+
+struct AccessRecord
+{
+   long int timeStamp;
+   char fileName[MAX_BUFFER];
+   char user[MAX_BUFFER];
+};
+
+struct Query
+{
+   long int startTime;
+   long int endTime;
+   char accessRecord[MAX_BUFFER];
+   int fileLength;
+   int results[MAX_RESULTS];
+   int resultCount;
+};
+
+void getFile (Query &q)
+{
+   cout << "Enter the access record file: ";
+   cin >> q.accessRecord;
+   cout << endl;
+
+}
+
+void getTime (Query &q)
+{
+   cout << "Enter the start time: ";
+   cin >> q.startTime;
+   cout << "Enter the end time: ";
+   cin >> q.endTime;
+
+}
+
+void readFile (Query &q, AccessRecord record[MAX_RESULTS])
+{
+   int q.fileLength = 0;
+
+   ifstream fin (q.accessRecord);
+
+   if (fin.fail ())
+   {
+      cout << "Unable to open: " << q.accessRecord << endl;
+   }
+
+   while (!fin.eof ())
+   {
+      fin >> record[q.fileLength].fileName;
+      fin >> record[q.fileLength].user;
+      fin >> record[q.fileLength].timeStamp;
+      q.fileLength++;
+   }
+
+   fin.close ();
+}
+
+void fileSearch (Query &q, AccessRecord record[MAX_RESULTS])
+{
+   q.resultCount = 0;
+
+   for (int i = 0; i < x.fileLength; ++i)
+   {
+      if (q.startTime <= record[i].timeStamp &&
+          q.endTime >= record[i].timeStamp)
+      {
+         q.results[q.resultCount] = i;
+         q.resultCount++;
+      }
+
+   }
+}
+
+void display (Query &q, AccessRecord record[MAX_RESULTS])
+{
+   cout << endl;
+   cout << "The following records match your criteria:" << endl;
+   cout << endl;
+   cout << setw (15) << "Timestamp" << setw (20) << "File" << setw (20)
+        << "User" << endl;
+   cout << "--------------- ------------------- -------------------" << endl;
+
+   for (int i = 0; i < q.resultCount; ++i)
+   {
+      cout << setw (15) << record[q.results[i]].timeStamp << setw (20)
+           << record[q.results[i]].fileName << setw (20)
+           << record[q.results[i]].user << endl;
+   }
+
+   cout << "End of records" << endl;
+}
 
 /**********************************************************************
  * Add text here to describe what the function "main" does. Also don't forget
  * to fill this out with meaningful text or YOU WILL LOSE POINTS.
  ***********************************************************************/
-int main()
+int main ()
 {
+   Query q;
+   AccessRecord record[MAX_RESULTS];
+
+   filePrompt (q);
+   timePrompt (q);
+   readFile (q, record);
+   fileSearch (q, record);
+   displayResults (q, record);
+
    return 0;
 }
