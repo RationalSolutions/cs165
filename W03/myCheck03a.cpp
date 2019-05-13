93438ee7baeb0068a06310b708817911142a7fd4
Coby Jenkins <cjenkins@parchment.com>
2019-05-13 14:12:04 -0700

    03 Prepare : Checkpoint A - Exceptions

--- W03/myCheck03a.cpp
+++ W03/myCheck03a.cpp
@@ -11,11 +11,41 @@
 #include <iostream>
 using namespace std;
 
+int prompt() throw (const string *)
+{
+   int num;
+
+   cout << "Enter a number: ";
+   cin >> num;
+   if (num < 0)
+   {
+      throw string("negative.");
+   }
+   if (num > 100)
+   {
+      throw string("greater than 100.");
+   }
+   if (num % 2)
+   {
+      throw string("odd.");
+   }
+   return num;
+}
+
 /**********************************************************************
  * Function: main
  * Purpose: This is the entry point and driver for the program.
  ***********************************************************************/
 int main()
 {
+   try
+   {
+      int num = prompt();
+      cout << "The number is " << num << "." << endl;
+   }
+   catch (const string * error)
+   {
+      cout << "Error: The number cannot be " << error << endl;
+   }
    return 0;
 }
