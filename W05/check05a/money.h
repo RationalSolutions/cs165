0f227dd7bd5776db5935b19c50560805b6f407cd
cr4ck3d <coby@azjenkins.com>
2019-06-06 10:59:13 -0700

    05 Prepare : Checkpoint A - Getters and Setters

--- W05/check05a/money.h
+++ W05/check05a/money.h
@@ -13,6 +13,10 @@ private:
 public:
    void prompt();
    void display() const;
+   int getDollars() const { return dollars; };
+   int getCents() const { return cents; };
+   void setDollars(int dollars) { this->dollars = abs(dollars); }
+   void setCents(int cents) { this->cents = abs(cents); };
 };
 
 #endif
