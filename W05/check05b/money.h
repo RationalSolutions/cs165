4300cfff3a42f610cf3f70b77050336f0fc87768
cr4ck3d <coby@azjenkins.com>
2019-06-06 12:46:39 -0700

    05 Prepare : Checkpoint B - Constructors

---
+++ W05/check05b/money.h
@@ -0,0 +1,28 @@
+/******************
+ * File: money.h
+ ******************/
+#ifndef MONEY_H
+#define MONEY_H
+
+#include <cstdlib>
+
+class Money
+{
+private:
+   int dollars;
+   int cents;
+
+public:
+   void prompt();
+   void display() const;
+   int getDollars() const { return dollars; };
+   int getCents() const { return cents; };
+   void setDollars(int dollars) { this->dollars = abs(dollars); }
+   void setCents(int cents) { this->cents = abs(cents); };
+
+   Money() { setDollars(0); setCents(0); }
+   Money(int dollars) { setDollars(dollars); setCents(0); }
+   Money(int dollars, int cents) { setDollars(dollars); setCents(cents); }
+};
+
+#endif
