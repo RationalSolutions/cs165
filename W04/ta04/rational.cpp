97673e92d788b135e564127bca574087f9da5134
cr4ck3d <coby@azjenkins.com>
2019-10-13 20:42:19 -0700

    W04 Commit w/ w4 assignment started

--- W04/ta04/rational.cpp
+++ W04/ta04/rational.cpp
@@ -16,11 +16,7 @@ void Rational::prompt()
    cout << "Bottom: ";
    cin >> bottom;
 }
-//TODO: if the value is an improper fraction (larger on top than bottom), it
-// displays it as a mixed number
-//    1)Identify improper fraction
-//    2)Identify whole# & remainder
-//    3)Update display format
+
 void Rational::display()
 {
    cout << top << "/" << bottom << endl;
@@ -31,20 +27,12 @@ void Rational::displayDecimal()
    float value = (float)top/bottom;
    cout << value << endl;
 }
-//TODO: Accept a new rational number & multiply it with the old rational
-// number
-//       1)Get another rational number
-//       2)Multiply the tops and the bottoms
-//       3)Update driver to reflect this functionality
+
 void Rational::multiplyBy()
 {
 
 }
-//TODO: reduce the rational numbers to their most basic equivalent (2/6 => 1/3)
-//       1)Identify if it can be reduced
-//       2)reduce the fraction if possible
-//       3)Update the object to reflect the updated value
-//       4)Modify rest of code to utilize reduce()
+
 void Rational::reduce(int top, int bottom)
 {
 
