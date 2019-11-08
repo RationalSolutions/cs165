de440fb883930ed02bbefb7545b972593e2aab60
cr4ck3d <coby@azjenkins.com>
2019-11-07 19:29:13 -0700

    W08 Skeet initial commit with UML + SCRATCH

---
+++ W08/skeet/scratch.cpp
@@ -0,0 +1,13 @@
+
+
+//TODO: create projectile base class for flying objects
+//TODO: create bullet class that extends projectile
+//TODO: create bird class that extends projectile
+//TODO: create toughBird that extends bird
+//TODO: create sacredBird that extends bird
+//TODO: Update makefile to support new classes created
+//TODO: Verify rifle - aim & fire
+//TODO: Verify bullet travel on screen
+//TODO: Verify "birds" travel on screen
+//TODO: MUST USE Bird* for the bird in the game.
+//TODO: Use Point & Velocity from moonLander
