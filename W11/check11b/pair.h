cbb1754f66df6b9fda7e9808313df457af3b39d8
cr4ck3d <coby@azjenkins.com>
2019-12-02 21:17:42 -0700

    W11 - check11b complete and submitted

--- W11/check11b/pair.h
+++ W11/check11b/pair.h
@@ -8,8 +8,25 @@
 #ifndef PAIR_H
 #define PAIR_H
 
-// TODO: Fill in this class
-// Put all of your method bodies right in this file
+#include <iostream>
+
+template <class T1, class T2>
+class Pair
+{
+private:
+   T1 first;
+   T2 second;
+
+public:
+   T1 getFirst() const { return first;}
+   T2 getSecond() const { return second;}
+   void setFirst(const T1 & value){first = value;}
+   void setSecond(const T2 & value){second = value;}
+
+   void display(){cout << first << " - " << second;}
+
+};
+
 
 
 #endif // PAIR_H
